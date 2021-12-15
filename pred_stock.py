a1c4520bd9d0a95fa0842202f5dfcbe396a9a218
cr2940 <36740525+cr2940@users.noreply.github.com>
2021-12-15 00:12:17 -0500

    remove buggy web scraping
    don't need as we use only few attributes

--- pred_stock.py
+++ pred_stock.py
@@ -31,31 +31,16 @@ def get_headers():
 def parse(ticker):
     url = "http://finance.yahoo.com/quote/%s?p=%s" % (ticker, ticker)
     response = requests.get(url, verify=True, headers=get_headers(), timeout=30)
-    # print("Parsing %s" % (url))
     parser = html.fromstring(response.text)
     summary_table = parser.xpath('//div[contains(@data-test,"summary-table")]//tr')
     summary_data = OrderedDict()
-    other_details_json_link = "http://query2.finance.yahoo.com/v10/finance/quoteSummary/{0}?formatted=true&lang=en-US&region=US&modules=summaryProfile%2CfinancialData%2CrecommendationTrend%2CupgradeDowngradeHistory%2Cearnings%2CdefaultKeyStatistics%2CcalendarEvents&corsDomain=finance.yahoo.com".format(ticker)
-    summary_json_response = requests.get(other_details_json_link, headers=get_headers())
     try:
-        json_loaded_summary = json.loads(summary_json_response.text)
-        summary = json_loaded_summary["quoteSummary"]["result"][0]
-        y_Target_Est = summary["financialData"]["targetMeanPrice"]['raw']
-        earnings_list = summary["calendarEvents"]['earnings']
-        eps = summary["defaultKeyStatistics"]["trailingEps"]['raw']
-        datelist = []
-        for i in earnings_list['earningsDate']:
-            datelist.append(i['fmt'])
-            earnings_date = ' to '.join(datelist)
-        if len(datelist)==0:
-            earnings_date = "None"
         for table_data in summary_table:
             raw_table_key = table_data.xpath('.//td[1]//text()')
             raw_table_value = table_data.xpath('.//td[2]//text()')
             table_key = ''.join(raw_table_key).strip()
             table_value = ''.join(raw_table_value).strip()
             summary_data.update({table_key: table_value})
-            summary_data.update({'1y Target Est': y_Target_Est, 'EPS (TTM)': eps,'Earnings Date': earnings_date, 'ticker': ticker,'url': url})
         return summary_data
     except ValueError:
         print("Failed to parse json response")
